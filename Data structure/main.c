2c5dc6999cb48a2d11664cccc2345f2dc2d490df
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-03-19 09:01:38 +0900

    Initial Commit

---
+++ Data structure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Data structure
+//
+//  Created by 홍재영 on 2021/03/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
