5b1b0594cf0c3b2bdb31da5b984d8211dc6af2e2
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-03-19 12:14:42 +0900

    ADD: 21/03/19 자료구조 stack

--- Data structure/main.c
+++ Data structure/main.c
@@ -6,9 +6,45 @@
 //
 
 #include <stdio.h>
+#define STACK_SIZE 5
+#include <stdbool.h>
+
+int stack[STACK_SIZE] = {0,};
+int top = -1;
+
+void PUSH(int value){
+    if (top>=STACK_SIZE){
+        printf("stack overflow\n");
+    }
+    else stack[++top] = value;
+}
+
+int POP(){
+    if (top==-1){
+        printf("stack underflow\n");
+        return false;
+    }
+    else{
+        printf("POP=>%d\n", stack[top]);
+        return stack[top--];
+    }
+}
+
+void _print(){
+    for (int i=top; i>=0; i--){
+        printf("stack[%d]=%d\n", i, stack[i]);
+    }
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    PUSH(1);
+    PUSH(2);
+    PUSH(3);
+    POP();
+    PUSH(4);
+    _print();
     return 0;
 }
+
